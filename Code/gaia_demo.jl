4c8f2888536a495f3e36901d9237daee444c0def
bjofilter <134409599+bjofilter@users.noreply.github.com>
2023-06-26 11:19:24 +0200

    Add files via upload

--- Code/gaia_demo.jl
+++ Code/gaia_demo.jl
@@ -10,26 +10,30 @@ include("ida.jl")
 
 
 
-# function get_effects_all_alphas(df, alphas, x, y)
-#     effects_all_alphas = DataFrame(Alpha = String[], Effect=Float64[], Multip=Float64[])
-#     for alpha in alphas
-#         est_CPDAG = pcalg(df, alpha, gausscitest)
-#         est_effects, multips = multiplicities(est_CPDAG, x, y, df)
-#         effects_all_alphas = add_all_effects!(effects_all_alphas, est_effects, multips, alpha)
+function get_effects_all_alphas(df, alphas, x, y)
+    effects_all_alphas = DataFrame(Alpha = String[], Effect=Float64[], Multip=Float64[])
+    for alpha in alphas
+        est_CPDAG = pcalg(df, alpha, gausscitest)
+        est_effects, multips = multiplicities(est_CPDAG, x, y, df)
+        effects_all_alphas = add_all_effects!(effects_all_alphas, est_effects, multips, alpha)
     
-#     end
-#     return effects_all_alphas
-# end
+    end
+    return effects_all_alphas
+end
 
 
 
-function add_all_effects!(est_effects, multips, alpha)
+function add_all_effects(est_effects, multips)
 
-    effs = DataFrame(Effect=Float64[], Multip=Float64[])
+    effs = DataFrame(Effect=Float64[], Probability=Float64[])
 
     for k in keys(est_effects)
-        effect = get(est_effects, k, 0)
+        if get(est_effects, k, 0) in effs[:,1]
+        n = get(est_effects, k, 0)
+        effs[effs.Effect .== n, :Probability] .= effs[effs.Effect .== n, :Probability] .+ get(multips, k, 0)/sum(values(multips))
+        else
             push!(effs, (get(est_effects, k, 0), get(multips, k, 0)/sum(values(multips))))
+        end
     end
 
     return effs
@@ -40,13 +44,22 @@ function gaia_demo(csv, x, y, alpha_val)
     data = DataFrame(csv)
     est_CPDAG = pcalg(data, alpha_val, gausscitest)
     est_effects, multips = multiplicities(est_CPDAG, x, y, data)
-    all_effects = add_all_effects!(est_effects, multips, alpha)
-    CSV.write("all_effects.csv", df)
+    print(est_effects)
+    all_effects = add_all_effects(est_effects, multips)
+    CSV.write("all_effects.csv", all_effects)
     return all_effects
 end
 
 
 # effects_all_alphas = get_effects_all_alphas(syntrea_data, alphas, x, y)
+
+
+
+csv = CSV.read("/Users/bjornfilter/Desktop/GAIA/csv/syndata.csv", DataFrame)
+x = 7
+y = 16
+alpha_val = 0.02
+all_effects = gaia_demo(csv, x, y, alpha_val)
 
 
 # nodelabel = 1:nv(net)
